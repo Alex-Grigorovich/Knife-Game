d573c72277aa5eaef33b4d12c3389bdfce7f1a52
Alex-Grigorovich <holdencolfield37@gmail.com>
2023-12-23 13:06:26 +0300

    Add sticking knives and background music.
    Add sticking knives and background music.

--- scripts/target.gd
+++ scripts/target.gd
@@ -1,6 +1,16 @@
 extends CharacterBody2D
+class_name Target
 
 var speed := PI
+const KNIFE_POSITION = Vector2(0, 180)
+@onready var items_container := $ItemsContainer
+
 
 func _physics_process(delta):
 	rotation += speed * delta
+
+func add_object_with_pivot(object: Node2D, object_rotation :float):
+	var pivot := Node2D.new()
+	pivot.rotation = object_rotation
+	pivot.add_child(object)
+	items_container.add_child(pivot)
